081baa53076ff92ed636769b4958d98a2a7bbc76
neil <neil.pandya.np@gmail.com>
2021-11-01 14:34:35 -0400

    switching to main.

--- Gemfile
+++ Gemfile
@@ -36,7 +36,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'better_errors'
 gem 'binding_of_caller'
-gem 'pry-rails'
+
 gem 'annotate'
 gem 'bcrypt'
 gem 'jquery-rails'
@@ -44,6 +44,7 @@ gem 'jquery-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,22 +1,22 @@
-import { createUser, deleteSession, createSession } from '../util/session_api_util';
+// import { createUser, deleteSession, createSession } from '../util/session_api_util';
 
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+// export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+// export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 
-const receiveCurrentUser = user => ({
-    type: RECEIVE_CURRENT_USER,
-    user,
-});
+// const receiveCurrentUser = user => ({
+//     type: RECEIVE_CURRENT_USER,
+//     user,
+// });
 
-const logoutCurrentUser = () => ({
-    type: LOGOUT_CURRENT_USER,
-});
+// const logoutCurrentUser = () => ({
+//     type: LOGOUT_CURRENT_USER,
+// });
 
-export const createUser = formUser => dispatch => postUser(formUser)
-    .then(user => dispatch(receiveCurrentUser(user)));
+// export const createUser = formUser => dispatch => postUser(formUser)
+//     .then(user => dispatch(receiveCurrentUser(user)));
 
-export const login = formUser => dispatch => createSession(formUser)
-    .then(user => dispatch(receiveCurrentUser(user)));
+// export const login = formUser => dispatch => createSession(formUser)
+//     .then(user => dispatch(receiveCurrentUser(user)));
 
-export const logout = () => dispatch => deleteSession()
-    .then(() => dispatch(logoutCurrentUser()));
+// export const logout = () => dispatch => deleteSession()
+//     .then(() => dispatch(logoutCurrentUser()));
