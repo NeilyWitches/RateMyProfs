6682946261718ed647b6bfbc85d4bcad4352ba36
neil <neil.pandya.np@gmail.com>
2021-11-02 17:31:00 -0400

    get image to appear, needs resizing.

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default ({ current_user, logout }) => {
     const display = current_user ? (
         <div className='nav-bar'>
-            {/* <img src={logo} alt="Logo" /> */}
+            <img src={window.logo} alt="Logo" />
             <h3>HEY, {current_user.first_name.toUpperCase()}</h3>
             <button onClick={logout}>Logout</button>
         </div>
