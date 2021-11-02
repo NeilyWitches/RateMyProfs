63f6a8f663f58411510b95d47913ceae8def00f0
neil <neil.pandya.np@gmail.com>
2021-11-02 18:15:16 -0400

    have logo redirect to home.

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 export default ({ current_user, logout }) => {
     const display = current_user ? (
         <div className='nav-bar'>
+            <Link to='/'><img src={window.logo} alt="Logo" className='logo' /></Link>
             <img src={window.logo} alt="Logo" className='logo'/>
             <h3>HEY, {current_user.first_name.toUpperCase()}</h3>
             <button onClick={logout}>Logout</button>
         </div>
     ) : (
         <div className='nav-bar'>
-            <img src={window.logo} alt="Logo" className='logo' />
+            <Link to='/'><img src={window.logo} alt="Logo" className='logo' /></Link>
             <Link className='session-form' to='/signup'>Sign Up</Link>
             <Link className='session-form' to='/login'>Log In</Link>
         </div>
