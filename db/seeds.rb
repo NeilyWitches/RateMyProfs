23d3b2c095d4a7fa0e8efd744043f16f82a774bf
neil <neil.pandya.np@gmail.com>
2021-11-03 18:23:24 -0400

    seed with profs, and prof_reviews.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,34 @@
 demo_user = User.create(email: 'demouser@demo.com', first_name: 'Demo User', last_name: 'Demo User')
 demo_user.password=('cupcake')
 demo_user.save!
+
+neil = Prof.create(first_name: 'Neil', last_name: 'Pandya', subject: 'Physics')
+george = Prof.create(first_name: 'George', last_name: 'Haramuniz', subject: 'Mathematics')
+greg = Prof.create(first_name: 'Greg', last_name: 'Sabin', subject: 'Management')
+
+neil_review_1 = ProfReview.create(body: "Dr. Pandya is just like 
+Ted from 'How I met your mother'. Coming from a communications 
+background I never thought I would be interested in physics, but this 
+professor really made me want to become an Einstein. This class is HARD. 
+H. A. R. D. but if you put the work in you'll know as much as Newton... 
+And then some!", klass: 'PHY185', grade: 'Rather not say')
+
+neil_review_2 = ProfReview.create(body: "Neil was an admirable professor, 
+with a great power of lucid exposition. His understanding is expressed in 
+how tangible he makes the course content to students. 
+I had no prior physics experience and he helped me grasp the concepts 
+with ease. It is a college physics class though and nothing less than 
+your best effort should be exerted.", klass: 'PHY185', grade: 'A+')
+
+george_review = ProfReview.create(body: "This is the best Ive professor 
+ever had. This course is online and he is so good with technology, so 
+easy and clear directions for students. He always explains everything so 
+clear, always open for questions, dont hesitate and sign up for his 
+class.", klass: 'MAT040', grade: 'Not sure yet')
+
+greg_review = ProfReview.create(body: "Corporate Finance was a great 
+class. We did cases each class that were fun and interactive. It made 
+learning easier. However, we also had to do a valuation project that 
+took at least 20 hours. Plus preparing all of those cases meant 
+there was A LOT of homework. But it was worth it. Best class ever!", 
+klass: 'MGMTE2000', grade: 'A-')
