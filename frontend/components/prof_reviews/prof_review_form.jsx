ceaf53a2ee96875fb78d02052411c4ea6d1a9803
neil <neil.pandya.np@gmail.com>
2021-12-13 21:10:13 -0500

    Added favicon

--- frontend/components/prof_reviews/prof_review_form.jsx
+++ frontend/components/prof_reviews/prof_review_form.jsx
@@ -110,8 +110,6 @@ class ProfReviewForm extends React.Component {
 
     componentDidMount() {
         this.setState({ characters: 350 })
-        // console.log(this.props)
-        // this.props.clearErrors();
     }
 
     render() {
