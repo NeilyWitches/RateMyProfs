94f75790dbedb61254f260cf2106814d109800b1
neil <neil.pandya.np@gmail.com>
2021-12-28 20:48:44 -0500

    no changes

--- app/controllers/api/school_ratings_controller.rb
+++ app/controllers/api/school_ratings_controller.rb
@@ -1,4 +1,4 @@
-class Api::SchoolRatingsController < ApplicationController
+\class Api::SchoolRatingsController < ApplicationController
     def index
         @school = School.includes(:profs, :prof_reviews, :school_ratings, :school_rating_likes).find(params[:schoolId])
         render :index
