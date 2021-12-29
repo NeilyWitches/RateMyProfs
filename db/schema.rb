db021c6a6d764cd668c83bfec3b934b539092a8c
neil <neil.pandya.np@gmail.com>
2021-12-28 21:32:50 -0500

    syntax error

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_12_28_155559) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "likes", force: :cascade do |t|
